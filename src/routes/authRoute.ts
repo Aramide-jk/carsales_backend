ab8b54fe84cfe91870999f62a0a60c4989584584
aramide-jk <aramjay29@gmail.com>
2025-12-23 13:37:26 +0100

    remove  protect to profile route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/profile", protect, getProfile);
+router.get("/profile", getProfile);
 
 export default router;
